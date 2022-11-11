c64bc6d01cbafbbb399a062cbeaaa5a8874870d1
michalchng <mike.13cc@gmail.com>
2022-11-10 19:36:41 -0500

    make site a hugo site

---
+++ content/posts/my-first-post.md
@@ -0,0 +1,6 @@
+---
+title: "My First Post"
+date: 2022-11-08T09:51:59-05:00
+draft: true
+---
+
--- index.js
+++
@@ -1,8 +0,0 @@
-function resize(){
-    img = document.getElementById("GP");
-
-    img.style.height = resize ? "250px" : "500px";
-    resize = resize ? false : true;
-}
-
-let resize = false;
